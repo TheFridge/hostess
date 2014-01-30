98ef645a72a2e31b93d7abbf0910dd101e34bd29
Rolen Le <rolentle@gmail.com>
2014-01-30 15:33:48 -0700

    set up omniauth boiler-plate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hostess::Application.config.secret_key_base = '3bb823c7377694cc8f7d89d8f22163e896c6c68c5804b1eeb46552588c8505c56043e72e7859962d758aa1cfc260222e19374f9715947678e07c27d7c6d07529'
